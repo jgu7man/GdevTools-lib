f543d8aa5b751c6b70c2f9911c5228d51a75424c
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2020-09-17 17:17:47 -0500

    función para crear duplicados indexados

--- commons/gdev-commons.service.ts
+++ commons/gdev-commons.service.ts
@@ -17,5 +17,48 @@ export class GdevCommonsService {
   setTitle(text) {
     this._title.setTitle(this.capitalize(text))
   }
+
+  async preventDuplicated( element: string | object, list: any[], key?: string ) {
+    var elementFinded
+    if ( typeof element === 'object' ) {
+      var newName = element[ key ]
+      elementFinded = list.find( item => item[ key ] === newName )
+      if ( elementFinded ) {
+        // [ 1, 2, 3 ].forEach(t => {
+        do {
+          let splitedName = newName.split( '(' )
+          let name = splitedName[ 0 ].trim();
+
+          let count = splitedName.length > 1 ?
+            splitedName[ 1 ].split( ')' )[ 0 ] : 1;
+
+          newName = `${ name } (${ +count + 1 })`;
+          elementFinded = list.find( item => item[ key ] === newName )
+
+          } while ( elementFinded )
+        // } )
+      }
+
+    } else {
+      elementFinded = list.find( item => item === element )
+      if ( elementFinded ) {
+        do {
+          let splitedName = element.split( '(' )
+          let name = splitedName[ 0 ]
+
+          let count = splitedName.length > 1 ?
+            splitedName[ 1 ].split( ')' )[ 0 ] : 1;
+
+          element = `${ name } (${ +count + 1 })`
+          elementFinded = list.find( t => t.name === element )
+
+        } while ( elementFinded )
+      }
+    }
+
+    
+    return key ? newName : newName
+
+  }
   
 }
