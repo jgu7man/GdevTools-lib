fc85bdf4459bb16683c42d50b1e8f19a210746b9
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2020-10-14 19:25:13 -0500

    acomodo de switch

--- text/gdev-text.service.ts
+++ text/gdev-text.service.ts
@@ -34,28 +34,7 @@ export class TextService {
 
     changeStringNumber(number: number) {
         switch (number) {
-            case 0:
-                return 'cero'
-            case 1:
-                return 'uno'
-            case 2:
-                return 'dos'
-            case 3:
-                return 'tres'
-            case 4:
-                return 'cuatro'
-            case 5:
-                return 'cinco'
-            case 6:
-                return 'seis'
-            case 7:
-                return 'siete'
-            case 8:
-                return 'ocho'
-            case 9:
-                return 'nueve'
-
-        }
+            case 0: return 'cero'; case 1: return 'uno'; case 2: return 'dos'; case 3: return 'tres'; case 4: return 'cuatro'; case 5: return 'cinco'; case 6: return 'seis'; case 7: return 'siete'; case 8: return 'ocho'; case 9: return 'nueve' }
     }
 
     generateRandomText(digits?: number) {
